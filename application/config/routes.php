da8b1284fae611fa153d361683623dc67912c821
ImamS2 <sudiyatmoimam@gmail.com>
2019-09-02 01:53:31 +0700

    commit route auth

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route["en_us/login"] = "auth/login";
+$route["en_us/logout"] = "auth/logout";
