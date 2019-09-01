dd16314504589e5eef49c7f55ca457fbc81e2d1e
ImamS2 <sudiyatmoimam@gmail.com>
2019-09-02 01:09:23 +0700

    autoload template, change default controller to home, and defined appname to ithenticate

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array(APPPATH.'third_party/ion_auth');
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session','form_validation','secure_library');
+$autoload['libraries'] = array('database','session','form_validation','secure_library','template');
 
 /*
 | -------------------------------------------------------------------
